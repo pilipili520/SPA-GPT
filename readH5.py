b1f50359db545922765bd36a45cb38bba28d64c7
pilipili520 <40008895+pilipili520@users.noreply.github.com>
2024-08-21 14:44:04 +0800

    Update readH5.py
    select h5

--- readH5.py
+++ readH5.py
@@ -1,21 +1,33 @@
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-with h5py.File('3-FPGA_noDelay_RSA.h5', 'r') as f:
-	for fkey in f.keys():
-		print(f[fkey], fkey)
+# list all h5 file
+h5_files = [f for f in os.listdir('.') if f.endswith('.h5')]
+print("Available H5 files:")
+for idx, file in enumerate(h5_files, 1):
+    print(f"{idx}: {file}")
 
-	print("======= (^o^) =========")
+print("======= (^o^) =========")
+# select a file
+choice = int(input("Enter the number of the file you want to plot (such as \"1: 6-AT89S52_ECC.h5\", input 1): ")) - 1
+file_path = h5_files[choice]
 
+# open h5 file
+with h5py.File(file_path, 'r') as file:
+    def print_keys(name, obj):
+        # print key
+        if isinstance(obj, h5py.Dataset):
+            print(name)
+    def plot_dataset(name, obj):
+        if isinstance(obj, h5py.Dataset) and 'for_paper' in name:
+            data = np.array(obj)
+            plt.figure()
+            plt.plot(data)
+            plt.title(name)
+            plt.show()
 
-	paper = f["traces"]["FPGA_RSA_noDelay_for_paper"]
-	plt.plot(paper)
-	plt.show()
-
-
-
-
-
-
-
+    # plot
+    file.visititems(print_keys)
+    file.visititems(plot_dataset)
