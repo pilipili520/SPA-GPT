d07587af06465b3b121b01d78d65177605ad5b3b
pilipili520 <40008895+pilipili520@users.noreply.github.com>
2024-08-21 14:42:26 +0800

    Update readH5.m
    user can choose

--- readH5.m
+++ readH5.m
@@ -1,3 +1,47 @@
-h5disp('1-CARD_RSA.h5');
-data = h5read('1-CARD_RSA.h5','/traces/CARD_RSA_for_paper');
-plot(data);
+files = dir('*.h5');
+disp('Available H5 files:');
+for i = 1:length(files)
+    fprintf('%d: %s\n', i, files(i).name);
+end
+
+% select a file
+disp '======= (^o^) =========';
+choice = input('Enter the number of the file you want to plot (such as \"1: 6-AT89S52_ECC.h5\", input 1): ');
+filename = files(choice).name;
+
+% read h5 info
+info = h5info(filename);
+
+% print details 
+stack = {info};
+prefixes = {''};
+
+while ~isempty(stack)
+    current_info = stack{end};
+    current_prefix = prefixes{end};
+    stack(end) = [];
+    prefixes(end) = [];
+    
+    if isfield(current_info, 'Groups')
+        for i = 1:length(current_info.Groups)
+            stack{end+1} = current_info.Groups(i);
+            prefixes{end+1} = [current_prefix '/' current_info.Groups(i).Name];
+            disp(['Group: ' current_prefix '/' current_info.Groups(i).Name]);
+        end
+    end
+    
+
+    if isfield(current_info, 'Datasets')
+        for i = 1:length(current_info.Datasets)
+            disp(['Dataset: ' current_prefix '/' current_info.Datasets(i).Name]);
+            if contains(current_info.Datasets(i).Name, 'for_paper')
+                data = h5read(filename, ['/' current_prefix '/' current_info.Datasets(i).Name]);
+                figure;
+                plot(data);
+                title(strrep(current_info.Datasets(i).Name, '_', ' '));
+            end
+            
+            
+        end
+    end
+end
